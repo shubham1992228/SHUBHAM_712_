a333d77db5a8d5c2c14e7713599c0451ef5ecda5
Kiraxop <97729246+Kiraxop@users.noreply.github.com>
2022-02-09 21:50:48 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -74,7 +74,7 @@ async def start_(client: Client, message: Message):
     ],
     
     
-@Client.on_message(commandpro(["/start", "/alive", "aditya"]) & filters.group & ~filters.edited)
+@Client.on_message(commandpro(["/start", "/alive"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/90054ebb925fdba1a1056.jpg",
